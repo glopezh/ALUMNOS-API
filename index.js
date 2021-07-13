c81879107ae1c42cef353c55c8fb309179f1d144
Rallge <geraldinelopz@gmail.com>
2021-07-13 12:39:27 +0200

    Trabajando con middlewares

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
-// haciendo apì rest
+// Haciendo el apì rest
 
 const express = require("express");
+const morgan = require("morgan");
 // Metemos en una constante para luego llamar a sus metodos
 const app = express();
 
 // Se levanta el servidor
-app.listen(3010,(err) => {
-if (err){
-  
-  console.log("No se ha podido levantar el servidor");
-  return;
-}
+app.listen(3010, (err) => {
+  if (err) {
+    console.log("No se ha podido levantar el servidor");
+    return;
+  }
   console.log("Servidor escuchando en el puerto 3010");
-
 });
 
 // Hasta aqui se hace el servidor
 // Ahora a hacer los middleware
 
-app.use((req,res,next)=>{
-console.log("Paso 1");
-next();
-});
-
-app.use((req, res, next) => {
-  console.log("Paso 2");
+const middleware1 = (req, res, next) => {
+  console.log("Paso 1");
   next();
-});
-
-app.use((req, res, next) => {
+};
+const middleware2 = (req, res, next) => {
+  console.log("Paso 2");
+  if (req.method === "GET") {
+    next();
+  } else {
+    res.json({ mensaje: "Me paro en el paso 2" });
+  }
+};
+const middleware3 = (req, res, next) => {
   console.log("Paso 3");
   next();
-});
+};
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(middleware1);
+app.use(middleware2);
+app.use(middleware3);
+
+app.get(
+  "/alumnos",
+  (req, res, next) => {
+    // No se consolea en el cliente
+    console.log("Paso /alumnos 1");
+    req.aula = 1;
+    next();
+  },
+  (req, res, next) => {
+    console.log("Paso /alumnos 2");
+    next();
+  },
+  (req, res, next) => {
+    const { aula } = req;
+    console.log("Paso /alumnos 3");
+    console.log(`Listado de alumnos del aula ${aula}`);
+    res.json({ alumnos: [] });
+  }
+);
